f0b165788555f02c133306cb85b21ff3b3599638
Danny Smith <hi@danny.is>
2025-07-31 18:16:54 +0100

    Add build optimisations

--- vite.config.ts
+++ vite.config.ts
@@ -13,6 +13,9 @@ export default defineConfig(async () => ({
       '@': path.resolve(__dirname, './src'),
     },
   },
+  build: {
+    chunkSizeWarningLimit: 600, // Prevent warnings for template's bundled components
+  },
   // Vite options tailored for Tauri development and only applied in `tauri dev` or `tauri build`
   //
   // 1. prevent vite from obscuring rust errors
